0b6b319f8aa0e21f6e12c59facd1d2a6d725b3e5
Moritz Schütz <moritz-schuetz@gmx.de>
2021-04-19 18:17:55 +0200

    Added Receiver

---
+++ Receiver.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMapTest
+{
+    class Receiver
+    {
+        static void Main()
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                Byte[] bytes = new byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Console.WriteLine("Idle...");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected");
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes("TEST");
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+            Console.Write("Acknowledge?");
+            Console.Read();
+        }
+    }
+}
---
+++ Sender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMapTest
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            Connect("localhost", "test");
+        }
+
+        static void Connect(String server, String message)
+        {
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Sent: {message}");
+
+                data = new Byte[256];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine($"Received: {responseData}");
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Please specify a valid Server: " + e);
+            }
+            catch ( SocketException e)
+            {
+                Console.WriteLine("Socket Exception: " + e);
+            }
+            Console.Write("Acknowledge?");
+            Console.Read();
+        }
+
+    }
+}
